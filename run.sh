b401ecb00fe985d9bfe33ed60e54f10426dc1372
smartestrobotdai <smartestrobotdai@gmail.com>
2023-11-07 13:20:26 +0000

    delete

--- run.sh
+++
@@ -1,2 +0,0 @@
-unset OPENAI_API_KEY
-OPENAI_API_KEY="" DEFAULT_MODEL="llama-2-13b-sth" DEFAULT_EMBEDDING_MODEL="all-mpnet-base-v2-local" DEFAULT_MEMORY_TYPE="MEMORY-SUMMARIZER" OPENAI_API_HOST="http://localhost:5000" npm run dev
